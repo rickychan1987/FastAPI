f6f7bc1535a607298b973335c2e1ba6cece6edf9
rickychan1987 <khaivanla1987@gmail.com>
2021-10-16 20:41:11 +0700

    Done List all Orders

--- pizza-delivery/database.py
+++ pizza-delivery/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 
-engine=create_engine('postgresql://postgres:<your_db_pass>@localhost/pizza_delivery',
+engine=create_engine('postgresql://postgres:1234@localhost/pizza_delivery',
     echo=True
 )
 
